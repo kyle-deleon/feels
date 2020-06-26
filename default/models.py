287cc78f94cd90864d805fe3f1c6062685d203ff
kyle-deleongit config --global user.email kydeleon10@gmail.comgit config --global --listgit config --global user.name kyle-deleon <kydeleon10@gmail.com>
2020-06-25 17:56:24 -0700

    added users and feels class to models

--- default/models.py
+++ default/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    user_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __repr__(self):
+        return f"<User: {self.id} - {self.first_name} - {self.last_name}>"
+
+class Feels(models.Model):
+    content = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey(User,related_name="has_feels", on_delete="CASCADE")
